c72253deb8171be42c8c59a9ecc5344f43601245
Tymur Foshch <antariuz@gmail.com>
2020-09-12 01:31:08 +0300

    Done: Thread example

--- src/multithreads/Main.java
+++ src/multithreads/Main.java
@@ -3,6 +3,9 @@ package multithreads;
 public class Main {
 
     public static void main(String[] args) {
+        //Thread
+        MyThread testTread = new MyThread();
+        testTread.start();
     }
 
 }
